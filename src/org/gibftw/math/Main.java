7ad2ea126d83dfab06f9f50e41b81370140676e9
GibFTW <gibsonpil@gmail.com>
2016-07-26 11:26:12 -0400

    Update Main.java

--- src/org/gibftw/math/Main.java
+++ src/org/gibftw/math/Main.java
@@ -3,14 +3,19 @@ package org.gibftw.math;
 import java.util.Scanner;
 
 public class Main {
+	public static int 1;
+	public static int 2;
+	public static int 3;
 	public static void main(String[] args) {
 		System.out.println("What is 2+2?");
 		Scanner num = new Scanner(System.in);
 		int numInt = num.nextInt();
 		if(numInt==4) { System.out.println("Correct!"); 
+		1 = 1
 		} else { 
 			
 			System.out.println("Wrong!");
+			1 = 0
 		
 		}
 		
@@ -18,18 +23,21 @@ public class Main {
 		Scanner numbjoe = new Scanner(System.in);
 		int finalnumb = numbjoe.nextInt();
 		if(finalnumb==1000) { System.out.println("Correct!");
+		2 = 1
 		
 		} else {
 			System.out.println("Wrong!");
+			2 = 0
 		}
 		
 		System.out.print("What is 25+32?");
 		Scanner numbjoe1 = new Scanner(System.in);
 		int finalnumb1 = numbjoe1.nextInt();
-		if(finalnumb1==57) { System.out.println("yes");
-		
+		if(finalnumb1==57) { System.out.println("Correct!");
+		3 = 1
 		} else {
-			System.out.println("no");
+			System.out.println("Wrong!");
+			3 = 0
 		
 				
 	}
