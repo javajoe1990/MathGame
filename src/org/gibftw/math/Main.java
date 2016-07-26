80de72c2e9f783122017d028b80add49209bceae
javajoe1990 <majorj086@gmail.com>
2016-07-26 11:27:56 -0400

    more stuff...

--- src/org/gibftw/math/Main.java
+++ src/org/gibftw/math/Main.java
@@ -3,19 +3,14 @@ package org.gibftw.math;
 import java.util.Scanner;
 
 public class Main {
-	public static int 1;
-	public static int 2;
-	public static int 3;
 	public static void main(String[] args) {
 		System.out.println("What is 2+2?");
 		Scanner num = new Scanner(System.in);
 		int numInt = num.nextInt();
 		if(numInt==4) { System.out.println("Correct!"); 
-		1 = 1
 		} else { 
 			
 			System.out.println("Wrong!");
-			1 = 0
 		
 		}
 		
@@ -23,27 +18,60 @@ public class Main {
 		Scanner numbjoe = new Scanner(System.in);
 		int finalnumb = numbjoe.nextInt();
 		if(finalnumb==1000) { System.out.println("Correct!");
-		2 = 1
 		
 		} else {
 			System.out.println("Wrong!");
-			2 = 0
 		}
 		
 		System.out.print("What is 25+32?");
 		Scanner numbjoe1 = new Scanner(System.in);
 		int finalnumb1 = numbjoe1.nextInt();
-		if(finalnumb1==57) { System.out.println("Correct!");
-		3 = 1
+		if(finalnumb1==57) { System.out.println("yes");
+		
 		} else {
-			System.out.println("Wrong!");
-			3 = 0
+			System.out.println("no");
 		
 				
 	}
+	
+	
+	System.out.print("What is 800+800?");
+		Scanner numbjoe2 = new Scanner(System.in);
+		int finalnumb2 = numbjoe2.nextInt();
+		if(finalnumb2==1600) { System.out.println("yes");
+		
+		} else {
+			System.out.println("no");
 }
+	System.out.print("What is 8+2?");
+		Scanner numbjoe3 = new Scanner(System.in);
+		int finalnumb3 = numbjoe3.nextInt();
+		if(finalnumb3==10) { System.out.println("yes");
+		
+		} else {
+			System.out.println("no");
+		}
 		
 		
 	}
 
 }
+	System.out.print("What is 123+5?");
+		Scanner numbjoe4 = new Scanner(System.in);
+		int finalnumb4 = numbjoe4.nextInt();
+		if(finalnumb4==128) { System.out.println("yes");
+		
+		} else {
+			System.out.println("no");
+		}
+		
+	System.out.print("What is 12+5?");
+		Scanner numbjoe5 = new Scanner(System.in);
+		int finalnumb5 = numbjoe5.nextInt();
+		if(finalnumb5==17) { System.out.println("yes");
+		
+		} else {
+			System.out.println("no");
+		}		
+		
+		
